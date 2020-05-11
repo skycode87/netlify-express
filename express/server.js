8747392cd27eb57459d43e88831386cf29d0fcec
projas <projas@kometsales.com>
2020-05-11 12:00:13 -0500

    ok

--- express/server.js
+++ express/server.js
@@ -4,19 +4,101 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+const PORT = process.env.PORT || 3050;
+
+// MySql
+
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-06.cleardb.net',
+  user: 'be6e71c3d14d08',
+  password: '08b8c58e',
+  database: 'heroku_5449df91db1eed4'
+});
+
+// Route
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
+
+// all customers
+app.get('/customers', (req, res) => {
+  const sql = 'SELECT * FROM customers';
+
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.get('/customers/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `SELECT * FROM customers WHERE id = ${id}`;
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.post('/add', (req, res) => {
+  const sql = 'INSERT INTO customers SET ?';
+
+  const customerObj = {
+    name: req.body.name,
+    city: req.body.city
+  };
+
+  connection.query(sql, customerObj, error => {
+    if (error) throw error;
+    res.send('Customer created!');
+  });
+});
+
+app.put('/update/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, city } = req.body;
+  const sql = `UPDATE customers SET name = '${name}', city='${city}' WHERE id =${id}`;
+
+  connection.query(sql, error => {
+    if (error) throw error;
+    res.send('Customer updated!');
+  });
+});
+
+app.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `DELETE FROM customers WHERE id= ${id}`;
+
+  connection.query(sql, error => {
+    if (error) throw error;
+    res.send('Delete customer');
+  });
+});
+
+// Check connect
+connection.connect(error => {
+  if (error) throw error;
+  console.log('Database server running!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
